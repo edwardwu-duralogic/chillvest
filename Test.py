51436c01559040da221d5cfbd4fd7ec779cc3256
Edward Wu <edmund.utd@gmail.com>
2025-12-20 23:18:54 -0700

    Initial project setup and data sampling pipeline

---
+++ Test.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+# Dummy training data
+X = np.array([[1], [2], [3], [4], [5]])
+y = np.array([2, 4, 5, 4, 5])
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+# Predictions
+y_pred = model.predict(X)
+
+# Plot
+plt.scatter(X, y, color="blue", label="Actual")
+plt.plot(X, y_pred, color="red", label="Prediction")
+plt.xlabel("X")
+plt.ylabel("y")
+plt.legend()
+plt.show()
